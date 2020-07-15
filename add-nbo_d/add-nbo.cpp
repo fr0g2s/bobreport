0d05c5e18b47cfb1e5b1f2ec62bcb831ca82e5c6
fr0g2s <turttle2s@naver.com>
2020-07-15 14:53:23 +0900

    gilgil

--- add-nbo_d/add-nbo.cpp
+++ add-nbo_d/add-nbo.cpp
@@ -3,6 +3,10 @@
 #include <stdlib.h>
 #include "nbo.h"	// getHexFromFile(), 
 
+extern bool errorFlag;
+
+bool errorCheck(){ return errorFlag;}
+
 int main(int argc, char *argv[])
 {
 	if(argc != 3){
@@ -13,16 +17,14 @@ int main(int argc, char *argv[])
 	}
 
 	uint32_t num1, num2, sum;
-
-/*
+	
 	num1 = getHexFromFile(argv[1]);
+	if(errorCheck()){ return 0; }
 	num2 = getHexFromFile(argv[2]);
+	if(errorCheck()){ return 0; }
+
 	sum = num1 + num2;
-
-	printf("%d(0x%x) + %d(0x%x) = %d(0x%x)", num1, num1, num2, num2, sum);
-*/
-	getHexFromFile(argv[1]);
-
+	printf("%u(0x%x) + %u(0x%x) = %u(0x%x)\n", num1, num1, num2, num2, sum, sum);
 
 	return 0;
 }
--- add-nbo_d/nbo.cpp
+++ add-nbo_d/nbo.cpp
@@ -1,13 +1,30 @@
 #include <stdint.h>	// for uint32_t..
 #include <stdio.h>
 #include <unistd.h>
+#include <stddef.h>
 #include "nbo.h"
 
 #define SIZE 4
 
-uint32_t ntoh_32(char *buf)
+bool errorFlag;
+
+uint32_t ntoh_32(uint32_t n)
 {
-	return (buf[0]&0xff000000);
+	return ( (n&0xff000000) >> 24 | (n&0x00ff0000) >> 8 | (n&0x0000ff00) << 8 | (n&0x000000ff) << 24 );
+}
+
+void dump(void *p)
+{
+	size_t n = 4; 
+	uint8_t *u8 = static_cast<uint8_t *> (p);
+	size_t i = 0;
+
+	while(true){
+		printf("%02X ", *u8++);
+		if(++i >= n) break;
+		if(i%8 == 0) printf("\n");
+	}
+	printf("\n");
 }
 
 uint32_t getHexFromFile(char *filename)
@@ -15,43 +32,50 @@ uint32_t getHexFromFile(char *filename)
 	FILE *fp;
 	uint32_t fileSize;
 	uint32_t num = 0;
+	uint32_t *p;
 	unsigned char buff[6] = {0,};
+	errorFlag = false;
 
 	if((fp = fopen(filename, "r")) == NULL){
-		fprintf(stderr, "[error] file open error\n");
+		errorFlag = true;
+		fprintf(stderr, "[error] %s file open failed\n", filename);
 		fprintf(stderr, "tell admin\n");
-		sleep(1);
+		return 0;
 	}
 
-	fread(buff, 5, 1, fp);
-	for(int8_t i=0;i<=4;i++){
-		printf("%d: 0x%X\n", i, buff[i]);
-	}
-	
+//	printf("=========== %s ============\n", filename);
+
+	fileSize = fread(buff, 4, sizeof(char), fp);	// primize that all file size is 4 bytes.
+	fclose(fp);
+
+/*	
 	// for test
-	printf("== fread(&buff[4~0], 1, 1, fp) == \n");
-	for(int8_t i=4;i>=0;i--){
+	printf("== fread(&buff[3~0], 1, 1, fp) == \n");
+	for(int8_t i=3;i>=0;i--){
 		fread(&buff[i], 1, 1, fp);
 		printf("%d: 0x%X\n", i, buff[i]);
 	}
 
 	
 	//for test
-	printf("== fread(&buff[0~4], 1, 1, fp) ==\n");
-	for(int8_t i=0;i<=4;i++){
+	printf("== fread(&buff[0~3], 1, 1, fp) ==\n");
+	for(int8_t i=0;i<4;i++){
 		fread(&buff[i], 1, 1, fp);
 		printf("%d: 0x%X\n", i, buff[i]);
 	}
 
-	if(fileSize > 4){
+	if(fileSize > 4){	// alert
 		fprintf(stderr, "file size more than 4 bytes.\n");
 		fprintf(stderr, "we just read 4 bytes from file\n");
 	} else if(fileSize < 4){
 		fprintf(stderr, "file size less then 4 bytes\n");
 		fprintf(stderr, "auto padding with \\x00\n");
 	}
+*/
 
-//	num = ntoh_32(buff);
+	p = reinterpret_cast<uint32_t *>(buff);
+//	dump(p);
+	num = ntoh_32(*p);
 
 
 	return num;
